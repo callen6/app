3f57067ff5f9ee0294aabb178b5819e3a7e8e6a5
Cori Allen <coriallen@gmail.com>
2013-10-27 15:17:06 -0400

    Fix error in omniauth initializer by adding comma, create migration for users.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"]
    {
-   		access_type: "offline"
+   		access_type: "offline",
       scope: "userinfo.email, userinfo.profile, plus.me, http://gdata.youtube.com",
       prompt:"select_account",
       image_aspect_ratio: "square",
