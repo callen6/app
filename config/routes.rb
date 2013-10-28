5472bac094ce84912665d3f65489a1453b16b653
Cori Allen <coriallen@gmail.com>
2013-10-27 23:11:15 -0400

    Add top nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ App::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'signout', to: 'sessions#destroy', as: 'signout'
  	get 'auth/failure', to: redirect('/')
+
 end
